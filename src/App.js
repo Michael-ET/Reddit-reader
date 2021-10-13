d8e71ea6c15d121a1950ae20d993cffa4d8c01f5
Michael <Michael.e-t@outlook.com>
2021-10-13 12:15:52 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import './App.css';
 import React, {useState, useEffect} from 'react'
 import Article from './components/article/Article'
-import SearchBar from './components/searchBar/searchBar'
+
 
 
 function App() {
  
   const [articles, setArticles] = useState([])
-  const [subReddit, setSubReddit] = useState(' ');
- 
+  const [subReddit, setSubReddit] = useState('news');
+
 
   useEffect(()=>{
+    
     fetch('https://www.reddit.com/r/'+ subReddit + '.json').then(res=>{
       if(res.status !== 200){
         console.log('error')
         return
       }
       res.json().then(data=>{
-        if (data !== null){
+
           setArticles(data.data.children)
           console.log(articles)
-        }
+        
       })
     })
   }, [subReddit])
+
+
+  const handleSubmit = (e) => {
+    setSubReddit(e.target.value)
+    console.log(e.target.value)
+  }
 
 
   return (
@@ -38,9 +45,9 @@ function App() {
                 type='text' 
                 className='searchBar' 
    
-                value={subReddit} 
               
-                onSubmit={e => setArticles(e.target.value)}
+            
+                onSubmit={handleSubmit}
             />
             <button className='button'>Search</button>
         </form>
